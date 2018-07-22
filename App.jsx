df1a13c2e3bb25e0ca38c2b61d12db7ed2f17fc4
mithunpalo <mithun.adhikary@prothom-alo.info>
2018-07-22 16:26:33 +0600

    Create Component

--- App.jsx
+++ App.jsx
@@ -4,7 +4,27 @@ class App extends React.Component {
    render() {
       return (
          <div>
-            Hello World!!!
+            <Header/>
+            <Content/>
+         </div>
+      );
+   }
+}
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Header</h1>
+         </div>
+      );
+   }
+}
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>Content</h2>
+            <p>The content text!!!</p>
          </div>
       );
    }
